343805752a6fe9429b7de6f8bd81334726181854
jacksondean17 <45220444+jacksondean17@users.noreply.github.com>
2020-10-17 02:55:15 -0600

    Basic main menu

---
+++ Assets/OnButtonClick.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class OnButtonClick : MonoBehaviour
+{
+
+    public GameObject latField;
+    public GameObject lonField;
+    public GameObject latWidthField;
+    public GameObject lonWidthField;
+    float lat;
+    float lon;
+    float latWidth;
+    float lonWidth;
+
+
+    float[] GetCoords()
+    {
+
+        Debug.Log("getting coords");
+        lat = float.Parse(latField.GetComponent<Text>().text);
+        lon = float.Parse(lonField.GetComponent<Text>().text);
+        Debug.Log("Got coords");
+        latWidth = float.Parse(latWidthField.GetComponent<Text>().text);
+        lonWidth = float.Parse(lonWidthField.GetComponent<Text>().text);
+        float[] retArray = new float[] { lat, lon, latWidth, lonWidth };
+        string s = "";
+        foreach (float f in retArray)
+        {
+            s += f.ToString() + " ";
+        }
+        Debug.Log(s);
+        return retArray;
+    }
+
+    public void PrintCity()
+    {
+        float[] coords;
+        try
+        {
+            // lat, lon, latWidth, lonWidth
+            coords = GetCoords();
+        }
+        catch (FormatException) // incorrect string
+        {
+            return;
+        }
+
+        CreateWorld(coords);
+    }
+
+    private void CreateWorld(float[] coords)
+    {
+
+    }
+}
