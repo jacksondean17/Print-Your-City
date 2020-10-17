02d1e3bf8f413c27e7e6e0663bcdcafb2ba01198
Alex Charters <alexcharters45@gmail.com>
2020-10-17 02:51:35 -0600

    Render all buildings with names, and adds a slider on the side for adjusting building size exageration.

---
+++ Assets/Scripts/GetMapResponse.cs
@@ -0,0 +1,125 @@
+﻿using Google.Maps.Coord;
+using Google.Maps.Event;
+using Google.Maps.Examples.Shared;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts
+{
+    /// <summary>
+    /// This example demonstrates a basic usage of the Maps SDK for Unity.
+    /// </summary>
+    /// <remarks>
+    /// By default, this script loads the Statue of Liberty. If a new lat/lng is set in the Unity
+    /// inspector before pressing start, that location will be loaded instead.
+    /// </remarks>
+    [RequireComponent(typeof(Google.Maps.MapsService))]
+    public class GetMapResponse : MonoBehaviour
+    {
+        [Tooltip("LatLng to load (must be set before hitting play).")]
+        public LatLng LatLng = new LatLng(40.6892199, -74.044601);
+        public int northSouthBound = 500;
+        public int eastWestBound = 500;
+        public UnityEngine.Material modeledMaterial;
+        public UnityEngine.Material extrudedMaterial;
+        private bool isLoaded = false;
+        public Slider slider;
+
+        /// <summary>
+        /// Use <see cref="MapsService"/> to load geometry.
+        /// </summary>
+        private void Start()
+        {
+            // Get required MapsService component on this GameObject.
+            Google.Maps.MapsService mapsService = GetComponent<Google.Maps.MapsService>();
+
+            // Set real-world location to load.
+            mapsService.InitFloatingOrigin(LatLng);
+
+            // Register a listener to be notified when the map is loaded.
+            mapsService.Events.MapEvents.Loaded.AddListener(OnLoaded);
+            mapsService.Events.ModeledStructureEvents.WillCreate.AddListener(WillCreateModeled);
+            mapsService.Events.ExtrudedStructureEvents.WillCreate.AddListener(WillCreateExtruded);
+
+            // Load map with default options.
+            Bounds bounds = new Bounds(Vector3.zero, new Vector3(northSouthBound, 0, eastWestBound));
+            mapsService.LoadMap(bounds, ExampleDefaults.DefaultGameObjectOptions);
+        }
+
+        /// <summary>
+        /// Example of OnLoaded event listener.
+        /// </summary>
+        /// <remarks>
+        /// The communication between the game and the MapsSDK is done through APIs and event listeners.
+        /// </remarks>
+        public void OnLoaded(MapLoadedArgs args)
+        {
+            isLoaded = true;
+            // The Map is loaded - you can start/resume gameplay from that point.
+            // The new geometry is added under the GameObject that has MapsService as a component.
+            Debug.Log(transform);
+            List<String> names = new List<String>();
+            foreach (Transform child in transform)
+            {
+                Debug.Log(child);
+                if (child.GetComponent<Google.Maps.Unity.ExtrudedStructureComponent>() != null)
+                {
+                    child.transform.localScale = new Vector3(2, 2, 2);
+                    names.Add(child.GetComponent<Google.Maps.Unity.ExtrudedStructureComponent>().GetMapFeature().MapFeatureMetadata.Name);
+                }
+            }
+
+            foreach (String name in names)
+            {
+                Debug.Log("Building Name: " + name);
+            }
+        }
+
+        public void OnSizeChange()
+        {
+            if (isLoaded)
+            {
+                float value = slider.value;
+                foreach (Transform child in transform)
+                {
+                    Debug.Log(child);
+                    if (child.GetComponent<Google.Maps.Unity.ExtrudedStructureComponent>() != null)
+                    {
+                        child.transform.localScale = new Vector3(value, value, value);
+                    }
+                }
+            }
+        }
+
+        public void WillCreateModeled(WillCreateModeledStructureArgs args)
+        {
+            Google.Maps.Feature.Style.ModeledStructureStyle.Builder newStyle = new Google.Maps.Feature.Style.ModeledStructureStyle.Builder();
+            newStyle.Material = modeledMaterial;
+
+            //Debug.Log(args.MapFeature.GameObjectName());
+            Debug.Log(args.MapFeature.MapFeatureMetadata.Name);
+            args.Style = newStyle.Build();
+        }
+
+        public void WillCreateExtruded(WillCreateExtrudedStructureArgs args)
+        {
+            Google.Maps.Feature.Style.ExtrudedStructureStyle.Builder newStyle = new Google.Maps.Feature.Style.ExtrudedStructureStyle.Builder();
+            newStyle.RoofMaterial = extrudedMaterial;
+            newStyle.WallMaterial = extrudedMaterial;
+
+            //Debug.Log(args.MapFeature.GameObjectName());
+            if (args.MapFeature.MapFeatureMetadata.Name == "")
+            {
+                args.Cancel = true;
+            }
+            else
+            {
+                Debug.Log(args.MapFeature.MapFeatureMetadata.Name);
+                args.Style = newStyle.Build();
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/MouseLookAt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLookAt : MonoBehaviour {
+    private bool locked = false;
+    private bool lastState = false;
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        bool currentState = Input.GetKey(KeyCode.Escape);
+        if (currentState && currentState!=lastState)
+        {
+            locked = !locked;
+        }
+        if (!locked)
+        {
+            Look();
+        }
+        lastState = currentState;
+    }
+
+    public float lookSpeed = 3;
+    private Vector2 rotation = Vector2.zero;
+    public void Look () // Look rotation (UP down is Camera) (Left right is Transform rotation)
+    {
+        rotation.y += Input.GetAxis ("Mouse X");
+        rotation.x += -Input.GetAxis ("Mouse Y");
+        Camera.main.transform.localRotation = Quaternion.Euler (rotation.x * lookSpeed, rotation.y * lookSpeed, 0);
+    }
+}
